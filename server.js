16a79a456ff24cc7ee32b59b3722e62d198a997e
marissiva <marissiva1723@gmail.com>
2025-05-31 15:04:52 +0530

    console changes

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server runs at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
