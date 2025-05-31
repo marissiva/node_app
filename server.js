57c274771952a8a8193e7977b6afa0337d510b35
marissiva <marissiva1723@gmail.com>
2025-05-31 16:22:24 +0530

    changed listening code in PORT, '0.0.0.0'

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => {
   res.send('Hey Welcome to the Node World');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
+  
